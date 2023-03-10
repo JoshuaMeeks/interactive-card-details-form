45ed39a6a8f293c6e744da5054eaf3ec2142707f
Your Name <jlmeeks587@gmail.com>
2023-03-10 11:43:17 -0500

    Remove node modules

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -4,16 +4,23 @@ import '../Form.css';
 export const Form = () => {
   return (
     <form>
-      <label htmlFor="">Cardholder Name</label>
-      <input type="text" placeholder='e.g. Jane Appleseed'/>
-      <label htmlFor="">Cardholder Number</label>
-      <input type="text" />
-      <label htmlFor="">Exp. Date</label>
-      <input type="text" />
-      <label htmlFor="">(MM/YY)</label>
-      <input type="text" />
-      <label htmlFor="">CVC</label>
-      <input type="text" />
+      <label htmlFor="name">
+        <span>Cardholder Name</span>
+        <input id="name" type="text" placeholder='e.g. Jane Appleseed'/>
+      </label>
+      <label htmlFor="number">
+        <span>Cardholder Number</span>
+        <input id="number" type="number" placeholder="e.g. 1234 5678 9123 0000" />
+      </label>
+      <label htmlFor="exp-date">
+        <span>Exp. Date (MM/YY)</span>
+        <input id="exp-date" type="number" placeholder="MM" />
+        <input id="exp-date" type="number" placeholder="YY" />
+      </label>
+      <label htmlFor="">
+        <span>CVC</span>
+        <input id="cvc" type="number" placeholder="e.g. 123" />
+      </label>
     </form>
   )
 }
