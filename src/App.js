65aecf0afe9f3f9e7361f66282352e1dc340b423
Your Name <jlmeeks587@gmail.com>
2023-03-13 10:31:45 -0400

    Import cardBack image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Form } from "./Components/Form";
+import cardBack from "./Resources/images/bg-card-back.png";
 
 function App() {
   return (
     <>
       <div className="styling-container"></div>
+      <img src={cardBack} alt="card back" />
       <Form />
     </>
   );
