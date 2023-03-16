f56ba870aeeed6e27c3fd3b59a92a7f958b98617
Your Name <jlmeeks587@gmail.com>
2023-03-16 12:35:01 -0400

    Change card images from img elements to background of div elements to make card text and logo easier to position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Form } from "./Components/Form";
 import cardBack from "./Resources/images/bg-card-back.png";
 import cardFront from "./Resources/images/bg-card-front.png";
+import cardLogo from "./Resources/images/card-logo.svg";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <img src={cardBack} alt="card back" className="card-back" />
       </div>
       <div className="card-front-container">
+        <img src={cardLogo} alt="card logo" className="card-logo" />
         <img src={cardFront} alt="card front" className="card-front" />
       </div>
       <div className="form-container">
